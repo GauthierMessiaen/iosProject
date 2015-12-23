d9bd5e2e564113850095594d15ed2a2cb297e580
Gauthier Messiaen <gauthiermessiaen@gmail.com>
2015-12-23 17:48:51 +0100

    search item

--- IOSProject/addFilmController.swift
+++ IOSProject/addFilmController.swift
@@ -1,22 +1,39 @@
 import Foundation
 import UIKit
 
-class addFilmController: UITableViewController {
+class addFilmController: UITableViewController, UISearchBarDelegate {
     @IBOutlet weak var table: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
-    var completeList: Array<Film> = [
+    var completeList = [
         Film(title: "Star Wars", description: "This is not the description you're looking for", score: 9, userScore: 10),
         Film(title: "Lord of the Rings", description: "and my axe,... i mean description", score: 10, userScore: 9),
         Film(title: "The worst movie", description: "yep, the wrst movie there is", score: 1, userScore: 3)
     ]
     
-    var filteredData = [Film]()
+    var searchActive = false
+    var filteredData : [Film] = []
     var resultSearchController = UISearchController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.tableView.reloadData()
+        searchBar.delegate = self
+    }
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        searchActive = true;
+    }
+    
+    func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchActive = false;
     }
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -24,7 +41,11 @@ class addFilmController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return completeList.count
+        if searchActive {
+            return filteredData.count
+        } else {
+            return completeList.count
+        }
     }
     
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
@@ -32,20 +53,33 @@ class addFilmController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("filmCell", forIndexPath: indexPath) as! FilmTableViewCell
-        let film = completeList[indexPath.row]
+        let cell = tableView.dequeueReusableCellWithIdentifier("filmCell") as! FilmTableViewCell
         
-        
-        if self.resultSearchController.active {
+        if searchActive {
             cell.titleLabel?.text = filteredData[indexPath.row].title
             cell.descriptionLabel?.text = filteredData[indexPath.row].description
-            return cell
         } else {
-            cell.titleLabel?.text = film.title
-            cell.descriptionLabel?.text = film.description
-            
-            return cell
+            cell.titleLabel?.text = completeList[indexPath.row].title
+            cell.descriptionLabel?.text = completeList[indexPath.row].description
         }
+        
+        return cell
+    }
+    
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        filteredData = completeList.filter({ (text) -> Bool in
+            let tmp: NSString = text.title
+            let range = tmp.rangeOfString(searchText, options: NSStringCompareOptions.CaseInsensitiveSearch)
+            return range.location != NSNotFound
+        })
+        if(filteredData.count == 0){
+            searchActive = false;
+        } else {
+            searchActive = true;
+        }
+        self.tableView.reloadData()
     }
     
 }
